516636bef05a6162715ff21640d03d4dceb570df
Supriya Dnyaneshwar Mulik <supriyamulik99@gmail.com>
2026-08-11 12:14:31 +0530

    Time: 0 ms (100.00%) | Memory: 60.9 MB (53.15%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -9,28 +9,39 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ #include<bits/stdc++.h>
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>& ans)
+    TreeNode* first;
+    TreeNode* prev;
+    TreeNode* middle;
+    TreeNode* last;
+
+    void inorder(TreeNode* root)
     {
         if(root == NULL) return;
-        inorder(root->left, ans);
-        ans.push_back(root->val);
-        inorder(root->right, ans);
-    } 
-    void fixTree(TreeNode* root, vector<int>& ans, int& index)
-    {
-            if(root == NULL) return;
-
-            fixTree(root->left, ans, index);
-            root->val = ans[index++];
-            fixTree(root->right, ans, index);
+        inorder(root->left);
+        if(prev!=NULL && root->val < prev->val)
+        {
+            if(first == NULL) {
+                first = prev;
+                middle = root;
+            }
+            else{
+                last = root;
+            }
+        }
+        prev = root;
+        inorder(root->right);
     }
     void recoverTree(TreeNode* root) {
-        vector<int> ans;
-        inorder(root, ans);
-        sort(ans.begin(), ans.end());
-        int index = 0;
-        fixTree(root, ans, index);
+        first = middle = last = NULL;
+        prev = new TreeNode(INT_MIN);
+        inorder(root);
+        if(first && last) swap(first->val , last->val);
+        else if(first && middle)
+        {
+            swap(first->val, middle->val);
+        }
     }
 };
