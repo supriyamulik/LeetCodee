c56dfef7cae95cb13e60c25d2cd8254863269074
Supriya Dnyaneshwar Mulik <supriyamulik99@gmail.com>
2026-08-17 19:35:51 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (27.91%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) 
+    {
+        int m = board.size();
+        int n = board[0].size();
+
+        vector<vector<int>> visited(m, vector<int>(n, 0));
+
+        queue<pair<int, int>> q;
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        for(int i=0; i<n; i++)
+        {
+            if(board[0][i] == 'O' && !visited[0][i])
+            {
+                visited[0][i] = 1;
+                q.push({0, i});
+            }
+            if(board[m-1][i] == 'O' && !visited[m-1][i])
+            {
+                visited[m-1][i] = 1;
+                q.push({m-1, i});
+            }
+        }
+
+        for(int i=0; i<m; i++)
+        {
+            if(board[i][0] == 'O' && !visited[i][0])
+            {
+                visited[i][0] = 1;
+                q.push({i, 0});
+            }
+            if(board[i][n-1] == 'O' && !visited[i][n-1])
+            {
+                visited[i][n-1] = 1;
+                q.push({i, n-1});
+            }
+        }
+
+        while(!q.empty())
+        {
+            int r = q.front().first;
+            int c = q.front().second;
+
+            q.pop();
+
+            for(int i=0; i<4; i++)
+            {
+                int nr = r+dr[i];
+                int nc = c+dc[i];
+
+                if(nr >= 0 && nr < m && 
+                   nc >= 0 && nc < n &&
+                   !visited[nr][nc] &&
+                   board[nr][nc] == 'O')
+                   {
+                    visited[nr][nc] = 1;
+                    q.push({nr,nc});
+                   }
+            }
+        }
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(board[i][j] == 'O' && !visited[i][j])
+            {
+                board[i][j] = 'X';
+            }
+        }
+    }
+  }
+};
