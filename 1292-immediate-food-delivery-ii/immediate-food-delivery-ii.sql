552b6596d1b37645e030193d1340955ad115ac84
Supriya Dnyaneshwar Mulik <supriyamulik99@gmail.com>
2026-08-27 09:06:15 +0530

    Time: 820 ms (23.36%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,2 @@
+select ROUND(SUM(IF(order_date = customer_pref_delivery_date, 1, 0))*100/ COUNT(DISTINCT customer_id),2) as immediate_percentage from Delivery where (customer_id, order_date) IN (
+select customer_id, MIN(order_date) as first_Order from Delivery group by customer_id);
